9d3abeced84f95e1bf0e752296c0e821a5821985
yepBozo <noah.pietrzak@covesd.org>
2024-11-12 16:49:31 +0000

    Updates

--- Noah/sqlitebrowser.py
+++ Noah/sqlitebrowser.py
@@ -5,14 +5,14 @@ cur = conn.cursor()
 
 while True:
 
-    command = input ('enter an SQlite command')
+    command = input ('enter an SQlite command: ')
 
     if command== "Done":
         cur.close()
         break 
-    elif command.startswith('SELECT'):
+    elif command.startswith("SELECT"):
         cur.execute(command)
-        print(cur.fetchall(command))
+        print(cur.fetchall())
     else :
         cur.execute(command)
 
