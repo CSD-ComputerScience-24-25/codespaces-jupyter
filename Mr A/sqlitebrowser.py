5104730a082cf38e04496cfa6d1198bf543adbbf
cozymentor <114101655+cozymentor@users.noreply.github.com>
2024-11-19 17:30:09 +0000

    SQLite stuff

--- Mr A/sqlitebrowser.py
+++ Mr A/sqlitebrowser.py
@@ -1,6 +1,7 @@
 import sqlite3
 
-conn = sqlite3.connect('Mr.A\db.sqlite')
+db = "MRA_Users.sqlite"
+conn = sqlite3.connect(db)
 cur = conn.cursor()
 
 while True:
@@ -12,9 +13,15 @@ while True:
         break
     elif command.startswith("SELECT"):
         cur.execute(command)
-        print(cur.fetchall())
+        for item in cur.fetchall():
+            print(item)
     else:
         cur.execute(command)
     
 
 conn.commit()
+
+"""SELECT User.name, Role.title, Course.title
+  FROM User JOIN Member JOIN Course JOIN Role
+  ON Member.user_id = User.id AND Member.course_id = Course.id AND Member.role = Role.id
+  ORDER BY Course.title, Role.id DESC, User.name """
