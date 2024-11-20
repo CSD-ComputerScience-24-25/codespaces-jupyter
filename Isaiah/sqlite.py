c22f62120acae52f7b4735feb2cd842b3a2855b1
issaor1 <bamskkii@gmail.com>
2024-11-20 16:05:35 +0000

    sqlite thingy IW

---
+++ Isaiah/sqlite.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+db = "WEBBtracks.sqlite"
+conn = sqlite3.connect(db)
+cur = conn.cursor()
+
+while True:
+    command = input("Enter an sqlite command: ")
+    if command == "Done":
+        cur.close
+        break
+    elif command.startswith("SELECT"):
+        cur.execute(command)
+        print(cur.fetchall()[0])
+    else:
+        cur.execute(command)
+
+conn.commit()
