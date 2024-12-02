cc0e829940a112ee59757371cdddfa4c6cd6cb12
yepBozo <noah.pietrzak@covesd.org>
2024-12-02 16:27:08 +0000

    updates to final

---
+++ Noah/Semester 1 Final/sqllite.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+#database
+db = ('crane.sqlite')
+
+conn = sqlite3.connect(db)
+
+cursor = conn.cursor()
+
+while True : 
+
+    command = input ('enter command')
+
+    if command == "Done" :
+        cursor.close()
+        break
+    elif command.startswith("SELECT"):
+        cursor.execute(command)
+        for thing in cursor.fetchall():
+            print(thing)
+    else :
+
+        cursor.execute(command)
+
+conn.commit()
